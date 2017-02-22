2963467c615fe38e1170bffba5616caafe68470f
Philip Kloc <philipgkloc@gmail.com>
2017-02-22 09:52:58 -0700

    Added ability to POST an item and display in a table afterwards.

--- lists/tests.py
+++ lists/tests.py
@@ -7,16 +7,12 @@ from lists.views import home_page
 # Create your tests here.
 class HomePageTest(TestCase):
 	
-	def test_root_url_resolves_to_home_page(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
-	
-	def test_home_page_returns_correct_html(self):
-		response = self.client.get('/')
-		
-		html = response.content.decode('utf8')
-		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do lists</title>', html)
-		self.assertTrue(html.endswith('</html>'))
-		
+	def test_uses_home_template(self):
+		response = self.client.get('/')		
 		self.assertTemplateUsed(response, 'home.html')
+
+	def test_can_save_a_POST_request(self):
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertIn('A new list item', response.content.decode())
+		self.assertTemplateUsed(response, 'home.html')
+		

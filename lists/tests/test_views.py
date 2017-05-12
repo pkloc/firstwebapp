a26c697a510185e470820f4ed2690a160e6084fd
Philip Kloc <philipgkloc@gmail.com>
2017-05-12 12:17:23 -0600

    Split out unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-
 from lists.models import Item, List
 
 # Create your tests here.
@@ -8,37 +7,7 @@ class HomePageTest(TestCase):
 	def test_uses_home_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
-			
 
-class ListAndItemModelsTest(TestCase):
-	
-	def test_saving_and_retrieving_items(self):
-		list_ = List()
-		list_.save()
-		
-		first_item = Item()
-		first_item.text = 'The first (ever) list item'
-		first_item.list = list_
-		first_item.save()
-		
-		second_item = Item()
-		second_item.text = 'Item the second'
-		second_item.list = list_
-		second_item.save()
-		
-		saved_list = List.objects.first()
-		self.assertEqual(saved_list, list_)
-		
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
-		
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-		
-		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-		self.assertEqual(first_saved_item.list, list_)
-		self.assertEqual(second_saved_item.text, 'Item the second')
-		self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
@@ -83,6 +52,7 @@ class NewListTest(TestCase):
 		new_list = List.objects.first()
 		self.assertRedirects(response, f'/lists/{new_list.id}/')
 
+		
 class NewItemTest(TestCase):
 
 	def test_can_save_a_POST_request_to_an_existing_list(self):
